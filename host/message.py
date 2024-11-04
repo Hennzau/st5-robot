ac579626eff8fb02b67582cee36a7d6a4c55ada3
Enzo Le Van <dev@enzo-le-van.fr>
2024-11-04 21:09:19 +0100

    state of progress

---
+++ host/message.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+
+import numpy as np
+import json
+
+from pycdr2 import IdlStruct
+from pycdr2.types import uint32, float32, uint8
+from typing import List
+
+@dataclass
+class RGBCamera(IdlStruct):
+    rgb: bytes
+    width: uint32
+    height: uint32
+
+@dataclass
+class LineMiddle(IdlStruct):
+    value: float32
+
+@dataclass
+class JoyStickController(IdlStruct):
+    axes: List[float32]
+    buttons: List[uint32]
+    balls: List[float32]
---
+++ raspberry/template.py
@@ -0,0 +1,134 @@
+# =======================
+# Import the necessary libraries
+# =======================
+
+import signal
+import time
+import threading
+
+import zenoh
+
+# =======================
+# Import specific libraries for this node
+# =======================
+
+import cv2
+
+import numpy as np
+
+# =======================
+# Import the message class
+# =======================
+
+from message import RGBCamera as RGBCameraMessage
+
+class Node:
+    def __init__(self):
+
+        # =======================
+        # Register signal handlers
+        # =======================
+
+        signal.signal(signal.SIGINT, self.ctrl_c_signal)
+        signal.signal(signal.SIGTERM, self.ctrl_c_signal)
+
+        self.running = True
+        self.mutex = threading.Lock()
+
+        # =======================
+        # Complete here with your own variables
+        # =======================
+
+
+        # =======================
+        # Create zenoh session
+        # =======================
+
+        config = zenoh.Config.from_file("raspberry/zenoh_config.json")
+        self.session = zenoh.open(config)
+
+        # =======================
+        # Create zenoh stop handler
+        # =======================
+
+        self.stop_handler = self.session.declare_subscriber("happywheels/stop", self.zenoh_stop_signal)
+
+        # =======================
+        # Complete here with your own pub/sub
+        # =======================
+
+        self.camera_publisher = self.session.declare_publisher("happywheels/camera")
+
+    def run(self):
+        while True:
+            # =======================
+            # Check if the node should stop
+            # =======================
+
+            self.mutex.acquire()
+            running = self.running
+            self.mutex.release()
+
+            if not running:
+                break
+
+            # =======================
+            # Do your own code here
+            # =======================
+
+            # =======================
+            # Complete here with your own message
+            # =======================
+
+            image = RGBCameraMessage(
+                rgb=bytes([]),
+                width=640,
+                height=480,
+                )
+
+            self.camera_publisher.put(RGBCameraMessage.serialize(image))
+
+        # =======================
+        # Close the node
+        # =======================
+
+        self.close()
+
+    def close(self):
+        # =======================
+        # Unregister stop handler
+        # =======================
+
+        self.stop_handler.undeclare()
+
+        # =======================
+        # Complete here with your own cleanup code
+        # =======================
+
+        self.camera_publisher.undeclare()
+
+        # =======================
+        # Close zenoh session
+        # =======================
+
+        self.session.close()
+
+    def ctrl_c_signal(self, signum, frame):
+        # Stop the node
+
+        self.mutex.acquire()
+        self.running = False
+        self.mutex.release()
+
+        # Put your cleanup code here
+
+    def zenoh_stop_signal(self, sample):
+        # Stop the node
+
+        self.mutex.acquire()
+        self.running = False
+        self.mutex.release()
+
+if __name__ == "__main__":
+    node = Node()
+    node.run()

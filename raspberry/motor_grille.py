57d79c50b9757743c3386f5861f637ebf5993297
Enzo Le Van <dev@enzo-le-van.fr>
2024-11-06 10:17:52 +0100

    fix

--- raspberry/motor_grille.py
+++ raspberry/motor_grille.py
@@ -269,7 +269,7 @@ class Node:
                 self.padding_timer = None
 
                 print("Padding ended : ready for manoeuver")
-                print(random.choices(self.intersections if self.intersections is not None else ["STOP"]))
+                print(random.choice(self.intersections if self.intersections is not None else ["STOP"]))
                 self.state = "90RIGHT"
 
         if self.timer is None and self.padding_timer is None and self.grace_timer is None:
